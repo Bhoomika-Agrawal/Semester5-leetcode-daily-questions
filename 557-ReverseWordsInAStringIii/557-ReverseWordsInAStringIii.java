2850601e9b0b2043e2a1e51cf113d77890dce802
Bhoomika-Agrawal <bhoomikaagrawal955@gmail.com>
2025-07-31 14:11:49 +0530

    Create 557-ReverseWordsInAStringIii

---
+++ 557-ReverseWordsInAStringIii/557-ReverseWordsInAStringIii.java
@@ -0,0 +1,26 @@
+// Last updated: 7/31/2025, 2:13:03 PM
+class Solution {
+    public String reverseWords(String s) {
+        int start=0;
+        int end=0;
+        char[] arr=s.toCharArray();
+        while(start<arr.length){
+            while(end<arr.length && arr[end]!=' '){
+                end++;
+            }
+            reverse(start,end-1,arr);
+            start=end+1;
+            end=start;
+        }
+        return new String(arr);
+    }
+    public static void reverse(int st,int en,char[] arr){
+        while(st<en){
+            char temp=arr[st];
+            arr[st]=arr[en];
+            arr[en]=temp;
+            st++;
+            en--;
+        }
+    }
+}
