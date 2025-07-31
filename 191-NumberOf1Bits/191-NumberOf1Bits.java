54e62d988c860f409c33789e0dc3416b3118c0f8
Bhoomika-Agrawal <bhoomikaagrawal955@gmail.com>
2025-07-31 14:13:10 +0530

    Create 191-NumberOf1Bits

---
+++ 191-NumberOf1Bits/191-NumberOf1Bits.java
@@ -0,0 +1,13 @@
+// Last updated: 7/31/2025, 2:14:25 PM
+class Solution {
+    public int hammingWeight(int n) {
+        int c=0;
+        while(n>0){
+            if(n%2==1){
+                c++;
+            }
+            n=n/2;
+        }
+        return c;
+    }
+}
