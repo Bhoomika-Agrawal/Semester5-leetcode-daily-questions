9c267cac5ff0d0e31de115de8898e97bcdcb3620
Bhoomika-Agrawal <bhoomikaagrawal955@gmail.com>
2025-07-31 14:14:33 +0530

    Create 46-Permutations

---
+++ 46-Permutations/46-Permutations.java
@@ -0,0 +1,25 @@
+// Last updated: 7/31/2025, 2:15:48 PM
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<Integer> ll=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        boolean[] board=new boolean[nums.length];
+        perm(board,nums,ans,ll);
+        return ans;
+    }
+    public static void perm(boolean[] board,int[] nums,List<List<Integer>> ans,List<Integer> ll){
+        if(ll.size()==nums.length){
+            ans.add(new ArrayList<>(ll));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(board[i]==false){
+                ll.add(nums[i]);
+                board[i]=true;
+                perm(board,nums,ans,ll);
+                ll.remove(ll.size()-1);
+                board[i]=false;
+            }
+        }
+    }
+}
