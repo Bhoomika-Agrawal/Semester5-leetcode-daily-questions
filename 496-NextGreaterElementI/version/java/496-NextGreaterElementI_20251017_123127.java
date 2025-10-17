deb2ea2bfb0bce1e5244670c9d5d6b8ac29021b9
Bhoomika-Agrawal <bhoomikaagrawal955@gmail.com>
2025-10-17 12:30:12 +0530

    Create 496-NextGreaterElementI

---
+++ 496-NextGreaterElementI/version/java/496-NextGreaterElementI_20251017_123127.java
@@ -0,0 +1,28 @@
+// Last updated: 10/17/2025, 12:31:27 PM
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        int[] nextGreater = new int[nums2.length]; 
+        Stack<Integer> st = new Stack<>(); 
+        for (int i = 0; i < nums2.length; i++) {
+            while (!st.isEmpty() && nums2[i] > nums2[st.peek()]) {
+                nextGreater[st.pop()] = nums2[i];
+            }
+            st.push(i);
+        }
+        while (!st.isEmpty()) {
+            nextGreater[st.pop()] = -1;
+        }
+
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            // find nums1[i] in nums2
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums2[j] == nums1[i]) {
+                    ans[i] = nextGreater[j];
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
